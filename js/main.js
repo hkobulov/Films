c5f671d80c625131637d18e6a66840b924242907
hkobulov <kobulov.2000@mail.ru>
2021-12-16 18:06:46 +0500

    add bookmark

--- js/main.js
+++ js/main.js
@@ -1,13 +1,90 @@
 var elForm = document.querySelector('.film-form');
 var elList = document.querySelector('.film-list');
 var films = movies.slice(0, 50);
-var elModal = document.querySelector('.main-modal');
-var elInput = document.querySelector('.film-input');
 var elCount = document.querySelector('.movies-count');
 var elRating = document.querySelector('.film-rating');
 var elSortByRating = document.querySelector('.sort-rating');
 var elSortByName = document.querySelector('.sort-name');
 var searchInput = document.querySelector('.film-name');
+var elGenreSelect = document.querySelector('.film-select-genre');
+var elModal = document.querySelector('.modal');
+var elBookMark = document.querySelector('.bookmark-list');
+var elFilmCardTemplate = document.querySelector('.film-card-temp').content;
+var elBookmarkTemplate = document.querySelector('.bookmark-temp').content;
+
+var normolizedFilmsList = films.map((item, index) => {
+    return {
+        id: String(index + 1).padStart(4, 0),
+        title: item.Title.toString(),
+        img_link: `http://i3.ytimg.com/vi/${item.ytid}/mqdefault.jpg`,
+        year: item.movie_year,
+        rating: item.imdb_rating,
+        video_link: `https://www.youtube.com/watch?v=${item.ytid}`,
+        genre: item.Categories.split('|')
+    }
+})
+
+var sortedByRatingName = (normolizedFilmsList) => {
+    elSortByRating.addEventListener('change', evt => {
+        
+        var ratingSelectValue = elSortByRating.value;
+        
+        if (ratingSelectValue == 'low_to_high') {
+            normolizedFilmsList = normolizedFilmsList.sort((a, b) => a.rating - b.rating);
+            
+        } else if (ratingSelectValue == 'high_to_low') {
+            normolizedFilmsList = normolizedFilmsList.sort((a, b) => b.rating - a.rating);        
+        }
+    })
+    
+    elSortByName.addEventListener('change', () => {
+        var nameSelectValue = elSortByName.value
+        
+        if (nameSelectValue == 'a_z') {
+            normolizedFilmsList = normolizedFilmsList.sort((a, b) => String(a.title).toLowerCase() > String(b.title).toLowerCase() ? 1 : -1)
+        } else if (nameSelectValue == 'z_a'){
+            normolizedFilmsList = normolizedFilmsList.sort((b, a) => String(a.title).toLowerCase() > String(b.title).toLowerCase() ? 1 : -1)
+        }
+    })
+}
+
+// Render genres
+
+var renderGenres = (moviesList, nodeArray) => {
+    var genresList = [];    
+    moviesList.forEach(item => {
+        item.genre.forEach(genre => {
+            var isGenreOK = !genresList.includes(genre);
+            if(isGenreOK){
+                genresList.push(genre)
+            }
+            
+        })
+    })
+    
+    genresList.sort()
+    
+    var elGenreOptionFragment = document.createDocumentFragment();
+    genresList.forEach(item => {
+        var newOption = document.createElement('option');
+        
+        newOption.textContent = item;
+        newOption.value = item;
+        elGenreOptionFragment.appendChild(newOption)
+    })
+    nodeArray.appendChild(elGenreOptionFragment)
+}
+renderGenres(normolizedFilmsList, elGenreSelect)
+
+function resultListFilms (title, rating, genre){
+    return normolizedFilmsList.filter(movie => {
+        var isGenreTrue = genre === 'All' || movie.genre.includes(genre)
+        
+        return movie.title.match(title) && movie.rating >= rating && isGenreTrue;
+    })
+}
+
+// Render movie
 
 function renderMovie(array, node){
     if(array.length > 0){
@@ -15,166 +92,97 @@ function renderMovie(array, node){
     } else {
         elCount.textContent = 'not found'
     }
+    
+    var newItemFragment = document.createDocumentFragment();
+    
     for(var film of array){
-        // New elements
-        var newItem = document.createElement('div');
-        var newCard = document.createElement('div');
-        var newImgCard = document.createElement('div');
-        var newImg = document.createElement('img');
-        var newCardBody = document.createElement('div');
-        var newHeadingArea = document.createElement('div');
-        var newHeading = document.createElement('h3');
-        var newYearArea = document.createElement('div');
-        var newYearImg = document.createElement('img')
-        var newYear = document.createElement('time');
-        var newRatingArea = document.createElement('div');
-        var newRatingImg = document.createElement('img');
-        var newRating = document.createElement('strong');
-        var newBtnArea = document.createElement('div');
-        var newLink = document.createElement('a');
-        var newBtn = document.createElement('button');
-        var newMark = document.createElement('button');
-        var newModalHeading = document.createElement('h3');
+        var cardItemClone = elFilmCardTemplate.cloneNode(true)
         
-        // Attribute
-        newItem.setAttribute('class', 'col-md-6');
-        newCard.setAttribute('class', 'card mb-3');
-        newImg.setAttribute('class', 'col-md-6');
-        newImg.setAttribute('src', `http://i3.ytimg.com/vi/${film.ytid}/mqdefault.jpg`);
-        newImg.setAttribute('class', 'w-100 rounded-top');
-        newCardBody.setAttribute('class', 'card-body');
-        newHeading.setAttribute('class', 'card-title h5');
-        newYearArea.setAttribute('class', 'mb-3');
-        newYearImg.setAttribute('src', './images/icon-calendar.png');
-        newYearImg.setAttribute('width', 25);
-        newYearImg.setAttribute('heigth', 25);
-        newRatingArea.setAttribute('class', 'mb-3 d-flex align-items-center');
-        newRatingImg.setAttribute('src', './images/icon-star.png');
-        newRatingImg.setAttribute('width', 25);
-        newRatingImg.setAttribute('heigth', 25);
-        newRating.setAttribute('class', 'text-dark')
-        newLink.setAttribute('href', `https://www.youtube.com/watch?v=${film.ytid}`);
-        newBtnArea.setAttribute('class', 'd-flex justify-content-evenly');
-        newBtn.setAttribute('type', 'button');
-        newBtn.setAttribute('data-bs-toggle', 'modal');
-        newBtn.setAttribute('data-bs-target', '#staticBackdrop');
-        newLink.setAttribute('class', 'btn btn-outline-primary');
-        newBtn.setAttribute('class', 'btn btn-outline-primary');
-        newMark.setAttribute('type', 'button');
-        newMark.setAttribute('class', 'btn btn-outline-success');
+        cardItemClone.querySelector('.film-card-heading').textContent = film.title;
+        cardItemClone.querySelector('.film-card-img').src = film.img_link;
+        cardItemClone.querySelector('.film-card-year').textContent = film.year;
+        cardItemClone.querySelector('.film-card-rating').textContent = film.rating;
+        cardItemClone.querySelector('.film-card-link').href = film.video_link;
+        cardItemClone.querySelector('.film-card-bookmark').dataset.id = film.id
         
-        // Text content
-        newHeading.textContent = film.Title;
-        newYear.textContent = film.movie_year;
-        newRating.textContent = film.imdb_rating;
-        newLink.textContent = 'Watch trailer';
-        newBtn.textContent = 'More info';
-        newMark.textContent = 'Bookmark';
-        
-        // Append
-        newImgCard.appendChild(newImg);
-        newItem.appendChild(newCardBody);
-        newHeadingArea.appendChild(newHeading);
-        newItem.appendChild(newHeadingArea);
-        newYearArea.appendChild(newYearImg);
-        newYearArea.appendChild(newYear);
-        newRatingArea.appendChild(newRatingImg);
-        newRatingArea.appendChild(newRating);
-        newBtnArea.appendChild(newLink);
-        newBtnArea.appendChild(newBtn);
-        newBtnArea.appendChild(newMark);
-        newCardBody.appendChild(newHeadingArea);
-        newCardBody.appendChild(newYearArea);
-        newCardBody.appendChild(newRatingArea);
-        newCardBody.appendChild(newBtnArea);
-        newCard.appendChild(newImgCard);
-        newCard.appendChild(newCardBody);
-        newItem.appendChild(newCard);
-        node.appendChild(newItem);
-        elModal.appendChild(newModalHeading);
+        newItemFragment.appendChild(cardItemClone)
+        // console.log(cardItemClone)
     }
+    elList.appendChild(newItemFragment)
 }
 
-renderMovie(films, elList)
+renderMovie(normolizedFilmsList, elList)
+
+// Bookmark
+
+function renderBookMark(bookmarkList){
+    var bookmarkFragment = document.createDocumentFragment();
+    
+    bookmarkList.forEach(item => {
+        var bookmarkClone = elBookmarkTemplate.cloneNode(true)
+        bookmarkClone.querySelector('.bookmark-heading').textContent = item.title;
+        bookmarkClone.querySelector('.bookmark-btn').dataset.id = item.id
+        bookmarkFragment.appendChild(bookmarkClone)
+    })
+    
+    elBookMark.appendChild(bookmarkFragment)
+}
+
+var bookmarkArrayList = []
+
+elList.addEventListener('click', evt => {
+    var bookmarkId = evt.target.dataset.id;
+    
+    
+    if(bookmarkId){
+        var findID = normolizedFilmsList.find(item => {
+            if(item.id == bookmarkId){
+                return item;
+            }
+        })
+        
+        var isBookmarkOK = bookmarkArrayList.includes(findID)
+        
+        if(!isBookmarkOK){
+            bookmarkArrayList.push(findID)
+        }
+    }
+    
+    elBookMark.innerHTML = null;
+    
+    renderBookMark(bookmarkArrayList)
+})
+
+elBookMark.addEventListener('click', evt => {
+    var btnID = evt.target.dataset.id
+    
+    if(btnID){
+        var findIndex = bookmarkArrayList.findIndex(item => {
+            if(item.id == btnID){
+                return item
+            }
+        })
+        
+        bookmarkArrayList.splice(findIndex, 1)
+        elBookMark.innerHTML = null;
+        
+        renderBookMark(bookmarkArrayList);
+    }
+})
+
+// Form menu
 
 elForm.addEventListener('submit', function(evt){
     evt.preventDefault()
     
-    var filteredMovieList = films;
-    
-    var nowTime = new Date();
-    var nowYear = nowTime.getFullYear();
     var searchValue = searchInput.value.trim().split(' ').join('|');
-    var inputValue = Number(elInput.value);
     var ratingValue = Number(elRating.value);
-    var ratingSelectValue = elSortByRating.value;
-    var nameSelectValue = elSortByName.value;
-    // Filter
+    var genreValue = elGenreSelect.value;
+    var searching = new RegExp (searchValue, 'gi');
     
-    
-    
-    if (searchValue){
-        var filteredMovieList = []
-        
-        var searching = new RegExp (searchValue, 'gi');
-        
-        films.forEach(film => {
-            var check = film.Title.toString().match(searching)
-            
-            if(check){
-                filteredMovieList.push(film)
-            }
-        })
-    }
-    
-    if (inputValue) {
-        filteredMovieList = filteredMovieList.filter(function(item){
-            if(item.movie_year >= nowYear - inputValue){
-                return true
-            }
-        })
-    }
-    
-    if (ratingValue) {
-        filteredMovieList = filteredMovieList.filter(function(item){
-            if(item.imdb_rating >= ratingValue){
-                return true
-            }
-        })
-    }
-    
-    if (ratingSelectValue == 'low_to_high') {
-        filteredMovieList = filteredMovieList.sort((a, b) => a.imdb_rating - b.imdb_rating);
-    } else if (ratingSelectValue == 'high_to_low') {
-        filteredMovieList = filteredMovieList.sort((a, b) => b.imdb_rating - a.imdb_rating);        
-    }
-    
-    if (nameSelectValue == 'a_z') {
-        filteredMovieList = filteredMovieList.sort((a, b) => String(a.Title).toLowerCase() > String(b.Title).toLowerCase() ? 1 : -1)
-    } else if (nameSelectValue == 'z_a'){
-        filteredMovieList = filteredMovieList.sort((b, a) => String(a.Title).toLowerCase() > String(b.Title).toLowerCase() ? 1 : -1)
-    }
-    
-    
-    // RenderGenre
-    // function renderGenre(films){
-    //     result = []
-    //     films.forEach(film => {
-    //         film.Categories.split('|').forEach(genre => {
-    //             if(!(result.includes(genre))){
-    //                 result.push(genre)
-    //             }
-    //         })
-    //     })
-    //     return result
-    // }
-    
-    
-    elSortByRating.addEventListener('click', function(){
-        
-    })
+    var endResultFilmsList = resultListFilms(searching, ratingValue, genreValue)
     
     elList.innerHTML = null;
     
-    renderMovie(filteredMovieList, elList)
+    renderMovie(endResultFilmsList, elList)
 })
